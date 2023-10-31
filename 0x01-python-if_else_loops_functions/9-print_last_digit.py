d548659e6e799e95d411721dc6c11de57f5e99dd
“Edwin <oriokie@icloud.com>
2023-11-01 00:19:37 +0300

    edited

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     sign = 1
     if number < 0:
         sign = -1
-    last_digit = (number % 10) * sign
+    last_digit = (abs(number % 10) * sign)
     print("{}".format(last_digit), end='')
     return last_digit
