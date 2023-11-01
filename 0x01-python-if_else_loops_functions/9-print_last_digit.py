13216c23ae24a63b32522234ae495ba491a0418b
“Edwin <oriokie@icloud.com>
2023-11-01 09:01:05 +0300

    lets do this

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     sign = 1
     if number < 0:
         sign = -1
-    last_digit = (sign *number) % 10
+    last_digit = (sign * number) % 10
     print("{}".format(last_digit), end='')
     return last_digit
