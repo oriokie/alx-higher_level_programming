91b1fccb08cfb9cb6cbf3e1cd00023a629212d56
“Edwin <oriokie@icloud.com>
2023-11-01 08:54:45 +0300

    added a sign

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     sign = 1
     if number < 0:
         sign = -1
-    last_digit = (abs(number % 10) * sign)
+    last_digit = (sign *(number % 10) * sign)
     print("{}".format(last_digit), end='')
     return last_digit
