10c2bd8001b2d50832ec07391ba8c3c4eefc8f1c
“Edwin <oriokie@icloud.com>
2023-11-02 21:03:27 +0300

    lets print

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv
+    i = len(arguments) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        for arg in range(1, i + 1):
+            print("{}: {}".format(arg, arguments[arg]))
