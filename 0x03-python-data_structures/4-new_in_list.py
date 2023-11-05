ed6133cb7d416efe5042465560a6f2948518612e
“Edwin <oriokie@icloud.com>
2023-11-05 13:18:46 +0300

    replaced the function

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     temp = my_list.copy()
     size = len(temp) - 1
     if idx < 0 or idx > size:
