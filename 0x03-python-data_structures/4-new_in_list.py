60ed9284815e8553f16c5199e7518b029a1fea2c
“Edwin <oriokie@icloud.com>
2023-11-05 13:15:24 +0300

    new_list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    temp = my_list.copy()
+    size = len(temp) - 1
+    if idx < 0 or idx > size:
+        return my_list
+    else:
+        temp[idx] = element
+        return temp
