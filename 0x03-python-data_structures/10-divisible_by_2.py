58b21d8226ffffafe702e5b77ef93891ef4b05d1
“Edwin <oriokie@icloud.com>
2023-11-06 10:49:10 +0300

    edited

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,8 +4,8 @@ def divisible_by_2(my_list=[]):
         return None
     len_list = len(my_list) - 1
     i = 0
-    new_list=[]
-    while i < len_list:
+    new_list = []
+    while i <= len_list:
         if my_list[i] % 2 == 0:
             new_list.append(True)
         else:
