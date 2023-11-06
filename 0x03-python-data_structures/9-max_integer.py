8401d40a9401ada278a65ee489b8aebe5255f7ce
“Edwin <oriokie@icloud.com>
2023-11-06 10:34:53 +0300

    edited

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    i = 0
     if not my_list:
         return None
-    else:
-        for item in my_list:
-            if item > i:
-                i = item
-    return i
+    max_item = my_list[0]
+    for item in my_list:
+        if item > max_item:
+            max_item = item
+    return max_item
