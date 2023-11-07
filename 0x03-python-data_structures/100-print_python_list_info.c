f3db6253187fdb4c9d74a4f1c40e5c064a8a2a20
“Edwin <oriokie@icloud.com>
2023-11-08 00:27:35 +0300

    lets do this

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "listobject.h"
-#include "object.h"
+#include <listobject.h>
+#include <object.h>
 /**
  * print_python_list_info - function that prints a Python list info
  *
