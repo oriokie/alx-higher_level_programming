dc7ed10d4b928eb8fa8978095fa582222d839c3c
Edwin <oriokie@gmail.com>
2023-11-21 08:55:42 -0800

    added the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This is a square Class  """
+
+
+class Square:
+    """
+    Class that defines a square
+
+    Attributes:
+        size (int): the dimension of the square
+
+    """
+    def __init__(self, size):
+        """
+        initialization of the class
+
+        Args:
+            size (int): the dimension of the square
+
+        """
+        self._size = size
