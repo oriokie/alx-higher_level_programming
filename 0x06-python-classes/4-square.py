0bc973127aaea0a20dead97df28adefd604f45ac
Edwin <oriokie@gmail.com>
2023-11-21 09:44:11 -0800

    square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -41,9 +41,9 @@ class Square:
             ValueError: if value is less than zero
 
         """
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
