c923b9b3356b66d57147e1da8f43522d9f43b5bd
“Edwin <oriokie@icloud.com>
2023-11-24 10:09:25 +0300

    interger account

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module adds two integers:
+    a and b must be integers or floats
+    a and b must be first casted to integers if they are float
+    No other module should be imported
+"""
+
+
+def add_integer(a, b=98):
+    """
+    This function adds two integers.
+
+    Args:
+        a (int or float): The first number
+        b (int or float): The second number. The default value is 98
+
+    Raises:
+        TypeError: if a or b is not an integer or float
+
+    Returns:
+        int: the addition of a and b
+
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
