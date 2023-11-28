df312bad03efe0eaf28abf08b971b2cd9d3b6171
“Edwin <oriokie@icloud.com>
2023-11-28 06:31:49 +0300

    done

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -102,3 +102,25 @@ class Rectangle:
         """Prints a message when an instance of Rectangle is deleted"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the biggest rectangle based on the area"""
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """
+        Returns a new Rectangle instance
+        with width == height == size
+
+        Args:
+            size (int): The size of the new square
+        """
+        return cls(size, size)
