1b5128e80557065b6fc84da37b76766c2fac9e2c
“Edwin <oriokie@icloud.com>
2023-11-28 12:12:33 +0300

    slots

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,9 +7,7 @@ Locked Class
 class LockedClass:
     """
     LockedClass with no class or object attribute
-    
     Attribute:
         first_name (str): first name
     """
     __slots__ = ['first_name']
-
