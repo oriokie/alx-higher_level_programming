bf69bf1758953a19768feaec9c790f35de05763b
“Edwin <oriokie@icloud.com>
2023-11-29 09:55:22 +0300

    addressed overflow

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -26,4 +26,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    result = int(a) + int(b)
+    if result == float('inf') or result == -float('inf'):
+        raise OverflowError
+    return int(result)
