4bb00d03d07fe76bcfd54b89ba233e8802da5513
“Edwin <oriokie@icloud.com>
2023-11-29 12:34:20 +0300

    added instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
         no_of_instance: number of instances
 
     """
-    no_of_instance = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """
@@ -32,7 +32,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.no_of_instance += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -98,5 +98,5 @@ class Rectangle:
 
     def __del__(self):
         """Prints a message when an instance of Rectangle is deleted"""
-        Rectangle.no_of_instance -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
