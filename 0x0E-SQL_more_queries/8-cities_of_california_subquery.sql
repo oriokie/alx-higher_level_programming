0d56bb2714cac316302d540058b45c3a05f0869c
Edwin K. Orioki <edwin.orioki@gmail.com>
2024-01-17 04:19:57 -0500

    added IN

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- lists all the cities of California in the database
 SELECT * FROM `cities`
-WHERE `state_id` = (
+WHERE `state_id` IN (
 	SELECT `id` FROM `states`
     WHERE `name` = "California")
 ORDER BY `id` ASC;
