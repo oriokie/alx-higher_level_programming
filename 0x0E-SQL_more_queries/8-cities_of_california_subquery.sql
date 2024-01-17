bf28ce26fc81dbf3aeb97e2b4ebfda2a2863702b
Edwin K. Orioki <edwin.orioki@gmail.com>
2024-01-17 04:21:43 -0500

    specified the columns

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- lists all the cities of California in the database
-SELECT * FROM `cities`
+SELECT `id`, `name`
+	FROM `cities`
 WHERE `state_id` IN (
 	SELECT `id` FROM `states`
     WHERE `name` = "California")
