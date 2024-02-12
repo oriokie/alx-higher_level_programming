42af7609f2daae950fd73d5a3e61ef6150205585
“Edwin <oriokie@icloud.com>
2024-02-12 09:45:15 +0300

    added function to print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // A script that prints three strings on different lines
 
-let stringLines = ["C is fun", "Python is cool", "JavaScript is amazing"]
+const stringLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < stringLines.length; i++) {
-	console.log(stringLines[i]);
+  console.log(stringLines[i]);
 }
