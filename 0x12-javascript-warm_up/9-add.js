c563eaeaefb3c1876ce03fba22a74808a0a8c09b
Edwin K. Orioki <edwin.orioki@gmail.com>
2024-02-12 05:07:03 -0500

    semistandard compliant

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // script that prints the addition of 2 integers
 
-function add(a, b) {
+function add (a, b) {
   const sum = a + b;
   return (sum);
 }
 const myNum1 = parseInt(process.argv[2]);
 const myNum2 = parseInt(process.argv[3]);
 
-console.log(add(myNum1, myNum2))
+console.log(add(myNum1, myNum2));
