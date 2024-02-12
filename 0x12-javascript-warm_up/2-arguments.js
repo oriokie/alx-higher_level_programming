45cb7fd0c923d72190a19d241840c1fd4aed4631
Edwin K. Orioki <edwin.orioki@gmail.com>
2024-02-12 04:08:36 -0500

    updated the string argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,7 +6,7 @@ const args = process.argv.length;
 if (args < 3) {
   console.log('No argument');
 } else if (args === 3) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
