568b8ee00b59719f923ad722c6e7eb981d725b8f
Edwin K. Orioki <edwin.orioki@gmail.com>
2024-02-17 17:48:38 -0500

    added a script to filter states that start with letter N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' Script that lists all states from the database '''
+import MySQLdb
+from sys import argv
+
+
+def main():
+    '''Creating connection to the db '''
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    ''' Creating a cursor '''
+    cur = db.cursor()
+
+    ''' Executing the query to list all the states '''
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+                ORDER BY ID ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    ''' close all cursor and database '''
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
