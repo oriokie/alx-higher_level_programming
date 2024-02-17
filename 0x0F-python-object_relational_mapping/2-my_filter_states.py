c0e47a6251e313e9eeebbd036fa0162f72cca4b7
Edwin K. Orioki <edwin.orioki@gmail.com>
2024-02-17 17:54:32 -0500

    added a filter to display states that match an argument provided by the user

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' Script that lists all states from the database '''
+import MySQLdb
+from sys import argv
+
+
+def main():
+    '''Creating connection to the db '''
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    filter_string = argv[4]
+    ''' Creating a cursor '''
+    cur = db.cursor()
+
+    ''' Executing the query to list all the states '''
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY ID ASC".format(filter_string)
+                )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    ''' close all cursor and database '''
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
