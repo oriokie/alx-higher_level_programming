4f6a9f7f7bbd4f451cb630bad28043d5ecf0ab55
Edwin K. Orioki <edwin.orioki@gmail.com>
2024-02-22 16:12:57 -0500

    added a script to delete

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module prints the state object with the name passed as an argument
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+def main():
+    """Main Function"""
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
+
+    """create tables defined in the models"""
+    Base.metadata.create_all(engine)
+
+    """create session factory to connect to the database"""
+    Session = sessionmaker(bind=engine)
+
+    """create session"""
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a')).all()
+    for state in states:
+        session.delete(state)
+
+    session.commit()
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
