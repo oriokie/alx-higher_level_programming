ab4b84a77e18d59fb365118a6c0c720f0a3d391b
“Edwin <oriokie@icloud.com>
2024-02-29 12:48:13 +0300

    try now

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL,
+# Bash script that takes in a URL, sends a request to that URL
 # and displays the size of the body of the response
-curl -sI "$1" | grep 'Content-Length' | cut -d' ' -f2
+content_length=$(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r')
+echo $content_length
